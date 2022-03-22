251b4a4135ef1b8d1db2651eea9086bb93f5090c
Charlie Bini <5003326+cbini@users.noreply.github.com>
2022-03-22 16:06:10 -0500

    Delete background-checks.sh

--- njdoe_sync/background-checks.sh
+++
@@ -1,5 +0,0 @@
-cd ~/datarobot/datagun
-./bin/qgtunnel ~/.pyenv/versions/datagun/bin/python ./datagun/extract.py --config ./datagun/config/njdoe.json
-
-cd ~/datarobot/njdoe-sync
-~/.pyenv/versions/njdoe-sync/bin/python ./njdoe_sync/background-checks.py
